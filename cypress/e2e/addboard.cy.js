8f9d43cd744fa21ebea8fbdc2327e7dc5e37acaf
Sac-tharan <stharan650@gmail.com>
2025-07-11 19:48:11 +0530

    add new board

---
+++ cypress/e2e/addboard.cy.js
@@ -0,0 +1,14 @@
+describe("Add New Board", () => {
+  it('should click "Create new board" and create a board', function () {
+    cy.fixture("addNewBoardTitleData").then((data) => {
+      const NewBoardTitle = data.NewBoardTitle;
+
+      cy.visit("http://localhost:3000/");
+      // Click the Create new board section
+      cy.get('[data-cy="create-board"]').click();
+
+      cy.get('[data-cy="new-board-input"]').type(NewBoardTitle);
+      cy.get('[data-cy="new-board-create"]').click();
+    });
+  });
+});
